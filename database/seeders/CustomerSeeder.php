dc90ec74e51ada7af42c512f28f7585c8e0987d1
Pablo Lupu <pablolupu2020@gmail.com>
2025-12-10 13:04:56 -0500

    seeeders

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -4,11 +4,14 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Customer;
+use Faker\Factory as Faker;
 
 class CustomerSeeder extends Seeder
 {
     public function run(): void
     {
+        $faker = Faker::create();
+
         // Ejemplos manuales
         Customer::create([
             'codigo'   => '12345678',
@@ -28,11 +31,19 @@ class CustomerSeeder extends Seeder
 
         // Automáticos con Faker
         for ($i = 0; $i < 20; $i++) {
+
+            // Generar código entre 8 y 11 dígitos
+            $length = rand(8, 11);
+            $codigo = '';
+            for ($x = 0; $x < $length; $x++) {
+                $codigo .= rand(0, 9);
+            }
+
             Customer::create([
-                'codigo'   => str_pad(rand(10000000, 99999999999), rand(8, 11), '0', STR_PAD_LEFT),
-                'nombres'  => fake()->name(),
-                'alias'    => fake()->userName(),
-                'telefono' => str_pad(rand(0, 999999999), 9, '0', STR_PAD_LEFT),
+                'codigo'   => $codigo,
+                'nombres'  => $faker->name(),
+                'alias'    => $faker->userName(),
+                'telefono' => $faker->numerify('#########'), // 9 dígitos
                 'state'    => (bool) rand(0, 1),
             ]);
         }
--- database/seeders/EngineSeeder.php
+++ database/seeders/EngineSeeder.php
@@ -4,19 +4,22 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Engine;
+use Faker\Factory as Faker;
 
 class EngineSeeder extends Seeder
 {
     public function run(): void
     {
-        // Motores de ejemplo manuales
+        $faker = Faker::create();
+
+        // Motores manuales
         Engine::create([
             'hp'     => '150HP',
             'tipo'   => 'Diesel',
             'marca'  => 'Cummins',
             'modelo' => 'QSB6.7',
-            'year'    => 2020,
-            'state'    => true,
+            'year'   => 2020,
+            'state'  => true,
         ]);
 
         Engine::create([
@@ -24,19 +27,20 @@ class EngineSeeder extends Seeder
             'tipo'   => 'Gasolina',
             'marca'  => 'Honda',
             'modelo' => 'GX630',
-            'year'    => 2022,
-            'state'    => true,
+            'year'   => 2022,
+            'state'  => true,
         ]);
 
-        // Motores aleatorios (Faker)
+        // Motores aleatorios seguros
         for ($i = 0; $i < 20; $i++) {
+
             Engine::create([
-                'hp'     => fake()->numberBetween(50, 500) . 'HP',
-                'tipo'   => fake()->randomElement(['Diesel', 'Gasolina', '2T', '4T']),
-                'marca'  => fake()->randomElement(['Yamaha', 'Honda', 'Caterpillar', 'Volvo Penta', 'Cummins']),
-                'modelo' => fake()->bothify('MOD###-??'),
-                'year'    => fake()->numberBetween(1990, 2025),
-                'state'    => (bool) rand(0, 1),
+                'hp'     => $faker->numberBetween(50, 500) . 'HP',
+                'tipo'   => $faker->randomElement(['Diesel', 'Gasolina', '2T', '4T']),
+                'marca'  => $faker->randomElement(['Yamaha', 'Honda', 'Caterpillar', 'Volvo Penta', 'Cummins']),
+                'modelo' => strtoupper($faker->bothify('MOD###-??')),
+                'year'   => $faker->numberBetween(1990, (int) date('Y')),
+                'state'  => $faker->boolean(),
             ]);
         }
     }
